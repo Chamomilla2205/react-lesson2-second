b4420118350002ba5dde92e379c64a06d41c18f8
freefireman <beluh.c.s@gmail.com>
2020-12-21 18:57:12 +0200

    little fix))

--- src/Components/AllPosts/AllPosts.js
+++ src/Components/AllPosts/AllPosts.js
@@ -2,8 +2,11 @@ import React, {Component} from 'react';
 import PostsComponent from "../posts/PostsComponent";
 import './AllPosts.css'
 class AllPosts extends Component {
+
     state = {posts: []}
+
 componentDidMount() {
+
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(value => value.json())
         .then(posts => {
@@ -12,7 +15,9 @@ componentDidMount() {
 }
 
     render() {
+
         let {posts} = this.state;
+
         return (
             <div>
                 <h1>ALL POSTS PAGE</h1>
